9fd6bbeb1a924aa194e0b55d961795cb048ec41c
LarryMawere <mawerelarry1@gmail.com>
2026-01-03 23:25:24 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/online_grades/OnlineGradesApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_grades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGradesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGradesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/online_grades/OnlineGradesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_grades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGradesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
