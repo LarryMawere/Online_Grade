ac43c80211a198f578fafe723ef8b5b0d1213e84
LarryMawere <mawerelarry1@gmail.com>
2026-01-04 01:57:32 +0100

    Add Flyway migration for users table

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Online Grades
+
+## Project Progress
+
+### 1. Project Setup
+-Created Spring Boot project
+-Configured Gradle
+-Connected Project to Github
+
+###2 Environment Setup
+-Verified app runs locally
---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       username TEXT NOT NULL UNIQUE,
+                       password TEXT NOT NULL,
+                       enabled BOOLEAN NOT NULL
+);
+CREATE TABLE users (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       username TEXT NOT NULL UNIQUE,
+                       password TEXT NOT NULL,
+                       enabled BOOLEAN NOT NULL
+);
